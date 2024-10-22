3e2923074e87f823535c3f983ebe9070e8853c11
Oleksii Shepel <oleksii.shepel@outlook.com>
2024-10-22 09:44:30 +0300

    updated behaviour subject

--- projects/streamix/src/lib/streams/behaviorSubject.ts
+++ projects/streamix/src/lib/streams/behaviorSubject.ts
@@ -1,8 +1,40 @@
-import { Subject } from './subject';
+import { Subscription } from "../abstractions";
+import { Subject } from "./subject";
 
 export class BehaviorSubject<T = any> extends Subject<T> {
+  private latestValue: T;
+
   constructor(private readonly initialValue: T) {
     super();
-    queueMicrotask(() => this.emissionAvailable = (() => this.isRunning.then(() => this.emissionAvailable).then(() => this.onEmission.process({ emission: { value: initialValue }, source: this })))());
+    this.latestValue = initialValue;
+
+    // Emit the initial value when the stream starts running
+    queueMicrotask(() => {
+      this.emissionAvailable = this.isRunning.then(() => {
+        return this.onEmission.process({ emission: { value: this.initialValue }, source: this });
+      });
+    });
+  }
+
+  // Override the next method to update the latest value and emit it
+  override async next(value: T): Promise<void> {
+    // Store the latest value
+    this.latestValue = value;
+
+    // Call the parent Subject's next method to handle the emission
+    return super.next(value);
+  }
+
+  // Ensure latest value is emitted when a new subscriber subscribes
+  override subscribe(callback?: ((value: T) => void) | void): Subscription {
+    // Emit the latest value immediately to the new subscriber
+    if (this.isRunning()) {
+      if (callback) {
+        callback(this.latestValue);
+      }
+    }
+
+    // Proceed with normal subscription
+    return super.subscribe(callback);
   }
 }
