6b17ebb91b09a853f19ef1f586d977600282ffee
actioncrew <sepeloleksij4@gmail.com>
2025-01-02 15:43:11 +0200

    corrections

--- projects/streamix/src/lib/hooks/defaultIfEmpty.ts
+++ projects/streamix/src/lib/hooks/defaultIfEmpty.ts
@@ -7,18 +7,6 @@ export const defaultIfEmpty = (defaultValue: any): StreamOperator => {
     let completed = false;
     const output = createSubject<any>(); // The stream that will emit values, including the default value
 
-    // const callback = function (this: Operator): (() => BusEvent) | void {
-    //   if (!hasEmitted) {
-    //     // If nothing has been emitted, emit the default value
-    //     let emission = createEmission({ value: defaultValue });
-    //     stream.next(emission);
-    //     stream.complete();
-    //   }
-    // };
-
-    // stream[hooks].onComplete.once(stream, callback); // Chain the callback to be triggered on stream completion
-
-    // Subscribe to the original stream
     const subscription = stream.subscribe({
       next: (value) => {
         hasEmitted = true; // Mark that the stream has emitted a value
@@ -32,31 +20,25 @@ export const defaultIfEmpty = (defaultValue: any): StreamOperator => {
       }
     });
 
-    // Ensure processedStream.complete() also triggers logic
-
     const originalComplete = output.complete.bind(output);
-    const delay = (ms: number): Promise<void> =>
-      new Promise((resolve) => {
-        const timerId = setTimeout(() => {
-          clearTimeout(timerId); // Ensure cleanup
-          resolve();
-        }, ms);
-      });
 
     output.complete = async () => {
       if(!completed) {
         completed = true;
-        await delay(0);
-        if (!hasEmitted) {
-          // If no value was emitted, emit the default value
-          output.next(defaultValue);
-        }
-        subscription.unsubscribe(); // Unsubscribe from the original stream
-        originalComplete();
+        return new Promise((resolve) => {
+          const timerId = setTimeout(() => {
+            clearTimeout(timerId);
+            if (!hasEmitted) {
+              output.next(defaultValue);
+            }
+            subscription.unsubscribe();
+            originalComplete();
+            resolve();
+          }, 0);
+        });
       }
     };
 
-    // Return the new stream with the default value logic applied
     return output;
   };
 };
