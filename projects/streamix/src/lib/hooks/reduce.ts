e76bbec608e37c9a50b624485d68e3eee106a3c4
actioncrew <sepeloleksij4@gmail.com>
2025-01-02 17:18:36 +0200

    reduce reimplemented

--- projects/streamix/src/lib/hooks/reduce.ts
+++ projects/streamix/src/lib/hooks/reduce.ts
@@ -1,28 +1,32 @@
-import { BusEvent, createEmission, createOperator, Emission, hooks, Operator, Stream } from '../abstractions';
+import { createEmission, hooks, Stream, StreamOperator } from '../abstractions';
 
-export const reduce = (accumulator: (acc: any, value: any) => any, seed: any): Operator => {
-  let boundStream: Stream;
+export const reduce = (accumulator: (acc: any, value: any) => any, seed: any): StreamOperator => {
   let accumulatedValue = seed;
 
-  const init = function (this: Operator, stream: Stream) {
-    boundStream = stream;
-    boundStream[hooks].onComplete.once(() => callback(this)); // Trigger the callback when the stream completes
-  };
+  return (stream: Stream): Stream => {
+    // Subscribe to the inputStream to start processing emissions
+    const subscription = stream.subscribe({
+      next: (value: any) => {
+        // Accumulate the value using the provided accumulator function
+        accumulatedValue = accumulator(accumulatedValue, value);
+      },
+      complete: () => {
+        // Emit the accumulated value once the stream completes
+        stream[hooks].onComplete.once(() => {
+          return () => {
+            const emission = createEmission({ value: accumulatedValue });
+            // Emit the result as a final emission
+            stream.next(emission);
+          };
+        });
+      }
+    });
 
-  const callback = (instance: Operator): (() => BusEvent) | void => {
-    // Emit the accumulated value once the stream completes
-    return () => ({ target: boundStream,  payload: { emission: createEmission({ value: accumulatedValue }), source: instance }, type: 'emission' });
-  };
+    // Store the subscription and clean up on finalize
+    stream[hooks].finalize.once(() => {
+      subscription.unsubscribe();
+    });
 
-  const handle = (emission: Emission): Emission => {
-    // Accumulate the value using the provided accumulator function
-    accumulatedValue = accumulator(accumulatedValue, emission.value!);
-    emission.phantom = true; // Mark the emission as phantom
-    return emission; // Return the emission
+    return stream;
   };
-
-  const operator = createOperator(handle);
-  operator.name = 'reduce';
-  operator.init = init;
-  return operator;
 };
