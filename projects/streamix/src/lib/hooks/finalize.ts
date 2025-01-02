0a336bb7d852794ed293b3094c37ef77fda3f8fc
actioncrew <sepeloleksij4@gmail.com>
2025-01-02 17:03:52 +0200

    corrections

--- projects/streamix/src/lib/hooks/finalize.ts
+++ projects/streamix/src/lib/hooks/finalize.ts
@@ -1,8 +1,6 @@
 import { hooks, Stream, StreamOperator } from '../abstractions';
 
 export const finalize = (callback: () => void | Promise<void>): StreamOperator => {
-
-  // Return the stream as required by StreamOperator type
   return (stream: Stream): Stream => {
     stream[hooks].finalize.chain(callback);
     return stream;
