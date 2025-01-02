ef12210aa2a11bfea7c4edf247c41945287e817d
actioncrew <sepeloleksij4@gmail.com>
2025-01-02 09:04:41 +0200

    added createSubscription

--- projects/streamix/src/lib/abstractions/subscription.ts
+++ projects/streamix/src/lib/abstractions/subscription.ts
@@ -1,3 +1,5 @@
+import { Emission } from "./emission";
+
 export interface Subscription {
   (): any;
   subscribed: number;
@@ -6,3 +8,25 @@ export interface Subscription {
   completed?: Promise<void>;
   unsubscribe(): void;
 }
+
+export const createSubscription = function (getValue: () => Emission, unsubscribe?: () => void): Subscription {
+  let currentValue: Emission | undefined = undefined;
+
+  const subscription = () => {
+    const emission = getValue();
+
+    if (!(emission.failed || emission.pending || emission.phantom)) {
+      currentValue = emission;
+    }
+
+    return currentValue?.value;
+  };
+
+  unsubscribe = unsubscribe ?? (function(this: Subscription) { this.unsubscribed = performance.now(); });
+
+  return Object.assign(subscription, {
+    subscribed: performance.now(),
+    unsubscribed: undefined,
+    unsubscribe
+  }) as any;
+};
