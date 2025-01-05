6daae35885aab80e3bcf77c15a540c121b360f67
actioncrew <sepeloleksij4@gmail.com>
2025-01-05 11:08:54 +0200

    corrections

--- projects/streamix/src/lib/operators/debounce.ts
+++ projects/streamix/src/lib/operators/debounce.ts
@@ -3,16 +3,20 @@ import { createEmission, createOperator, Emission, eventBus, Operator, Subscriba
 export const debounce = (time: number): Operator => {
   let timeoutId: any;
 
-  const handle = (emission: Emission, source: Subscribable): Emission => {
+  const handle = function (this: Operator, emission: Emission, source: Subscribable): Emission {
     clearTimeout(timeoutId); // Clear any previous debounce timer
+    const debounced = createEmission({ value: emission.value });
+
     timeoutId = setTimeout(() => {
+      emission.link(debounced);
       eventBus.enqueue({
         target: source,
-        payload: { emission: createEmission({ value: emission.value }), source },
+        payload: { emission: debounced, source: this },
         type: 'emission',
       }); // Emit the debounced value
+      emission.finalize();
     }, time);
-    emission.phantom = true; // Mark as delayed
+    emission.pending = true; // Mark as delayed
     return emission;
   };
 
