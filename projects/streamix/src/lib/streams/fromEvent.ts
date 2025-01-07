6d4294ca196fcf2daa925d74f349bec1f7d9fdc9
actioncrew <sepeloleksij4@gmail.com>
2025-01-07 11:46:35 +0200

    added missed name parameter to createStream

--- projects/streamix/src/lib/streams/fromEvent.ts
+++ projects/streamix/src/lib/streams/fromEvent.ts
@@ -1,22 +1,37 @@
-import { createEmission, createStream, flags, internals, Stream } from '../abstractions';
+import { createEmission, createStream, flags, internals, Stream, Subscription } from '../abstractions';
 
 export function fromEvent<T = any>(target: EventTarget, eventName: string): Stream<T> {
   let listener!: (event: Event) => void;
 
-  // Create the stream using createStream
-  const stream = createStream<T>('fromEvent', async function(this: Stream<T>): Promise<void> {
-    listener = async (event: Event) => {
-      if (this[flags].isRunning) {
-        this.next(createEmission({ value: event }));
-      }
-    };
-
-    target.addEventListener(eventName, listener);
-
+  const run = async function(this: Stream<T>): Promise<void> {
+    // Wait for completion
     await this[internals].awaitCompletion();
 
     target.removeEventListener(eventName, listener);
-  });
+  };
+
+  // Create the stream using createStream
+  const stream = createStream<T>('fromEvent', run);
+  const originalSubscribe = stream.subscribe.bind(stream); // Store the original start method
+
+  stream.subscribe = function(this: Stream<T>, params?: any): Subscription {
+    // Call the original start method
+    let subscription = originalSubscribe(params);
+
+    if (!listener) {
+      listener = async (event: Event) => {
+        if (this[flags].isRunning) {
+          // Emit the event to the stream
+          this.next(createEmission({ value: event }));
+        }
+      };
+
+      // Add the event listener to the target
+      target.addEventListener(eventName, listener);
+    }
+
+    return subscription;
+  };
 
   return stream;
 }
