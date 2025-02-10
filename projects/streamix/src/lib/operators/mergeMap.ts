870007221734af3c031685ce4d69ed2a0bca1980
actioncrew <sepeloleksij4@gmail.com>
2025-02-10 15:37:45 +0200

    corrections

--- projects/streamix/src/lib/operators/mergeMap.ts
+++ projects/streamix/src/lib/operators/mergeMap.ts
@@ -1,60 +1,31 @@
-import { createStreamOperator, Stream, StreamOperator } from "../abstractions";
-import { createSubject } from "../streams/subject";
+import { createStreamOperator, Stream, StreamOperator } from '../abstractions';
+import { createSubject } from '../streams/subject';
 
 export function mergeMap<T, R>(project: (value: T) => Stream<R>): StreamOperator {
-  const operator = (input: Stream<T>): Stream<R> => {
+  return createStreamOperator('mergeMap', (input: Stream<T>): Stream<R> => {
     const output = createSubject<R>();
-    let isOuterComplete = false;
-    let activeInnerStreams = 0; // Track active inner streams
 
-    // Async generator to process inner streams concurrently
-    const processInnerStream = async (innerStream: Stream<R>) => {
-      try {
-        for await (const emission of innerStream) {
-          output.next(emission.value!); // Forward value from inner stream
-        }
-      } catch (err) {
-        output.error(err);
-      } finally {
-        activeInnerStreams -= 1;
-        if (isOuterComplete && activeInnerStreams === 0) {
-          output.complete(); // Complete the output stream when all inner streams are processed
-        }
-      }
-    };
-
-    // Iterate over the input stream using async iterator
     (async () => {
       try {
         for await (const emission of input) {
-          const innerStream = project(emission.value!); // Project input to inner stream
-          activeInnerStreams += 1;
-          processInnerStream(innerStream); // Process the inner stream concurrently
+          const innerStream = project(emission.value!);
+
+          // Handling the inner stream
+          (async () => {
+            try {
+              for await (const innerEmission of innerStream) {
+                output.next(innerEmission.value!);
+              }
+            } catch (err) {
+              output.error(err); // Propagate the error from inner stream
+            }
+          })();
         }
       } catch (err) {
-        output.error(err);
-      } finally {
-        // If outer stream is complete and there are no active inner streams, complete output stream
-        if (isOuterComplete && activeInnerStreams === 0) {
-          output.complete();
-        }
+        output.error(err); // Propagate error from the outer stream if it occurs
       }
     })();
 
-    // Handling the outer stream's completion
-    input.subscribe({
-      next: () => {}, // This is handled by the async iterator already
-      error: (err) => output.error(err),
-      complete: () => {
-        isOuterComplete = true;
-        if (activeInnerStreams === 0) {
-          output.complete(); // Complete the output stream if all inner streams are done
-        }
-      },
-    });
-
     return output;
-  };
-
-  return createStreamOperator('mergeMap', operator);
+  });
 }
